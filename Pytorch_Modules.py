f7d3c24de2c1082b00dc565fd6406f986553ee17
pa0501 <74268276+pa0501@users.noreply.github.com>
2023-08-22 13:45:14 +0200

    Add files via upload

--- Pytorch_Modules.py
+++ Pytorch_Modules.py
@@ -8,7 +8,7 @@ from keras.layers import Layer
 
 
 def autopad(k, p=None, d=1):  # kernel, padding, dilation
-    # Pad to 'same' shape outputs
+    # Pad to 'same' shape outputs: k = 1: p = 0, k = 3: p = 1
     if d > 1:
         k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
     if p is None:
@@ -30,7 +30,50 @@ class Conv(nn.Module):
 
     def forward_fuse(self, x):
         return self.act(self.conv(x))
+    
+class Bottleneck(nn.Module):
+    # Standard bottleneck
+    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):  # ch_in, ch_out, shortcut, groups, kernels, expand
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, k[0], 1)
+        self.cv2 = Conv(c_, c2, k[1], 1, g=g)
+        self.add = shortcut and c1 == c2
 
+    def forward(self, x):
+        return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
+    
+class C2f(nn.Module):
+    # CSP Bottleneck with 2 convolutions
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+
+    def forward(self, x):
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+    
+class SPPF(nn.Module):
+    # Spatial Pyramid Pooling - Fast (SPPF) layer for YOLOv5 by Glenn Jocher
+    def __init__(self, c1, c2, k=5):  # equivalent to SPP(k=(5, 9, 13))
+        super().__init__()
+        c_ = c1 // 2  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c_ * 4, c2, 1, 1)
+        self.m = nn.MaxPool2d(kernel_size=k, stride=1, padding=k // 2)
+
+    def forward(self, x):
+        x = self.cv1(x)
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')  # suppress torch 1.9.0 max_pool2d() warning
+            y1 = self.m(x)
+            y2 = self.m(y1)
+            return self.cv2(torch.cat((x, y1, y2, self.m(y2)), 1))
+        
 # heads
 class DFL(nn.Module):
     # Integral module of Distribution Focal Loss (DFL) proposed in Generalized Focal Loss https://ieeexplore.ieee.org/document/9792391
@@ -43,8 +86,9 @@ class DFL(nn.Module):
 
     def forward(self, x):
         b, c, a = x.shape  # batch, channels, anchors (old height, width)
-        #return self.conv(x.view(b, 4, self.c1, a).transpose(2, 1).softmax(1)).view(b, 4, a)
-        return self.conv(x.view(b, self.c1, 4, a).softmax(1)).view(b, 4, a)
+        return self.conv(x.view(b, 4, self.c1, a).transpose(2, 1).softmax(1)).view(b, 4, a)
+        #return self.conv(x.view(b, self.c1, 4, a).softmax(1)).view(b, 4, a)
+        #return self.conv(x.view(b, 4, self.c1, a).transpose(2, 1).softmax(1))
     
 def make_anchors(feats, strides, grid_cell_offset=0.5):
     """Generate anchors from features."""
@@ -56,7 +100,8 @@ def make_anchors(feats, strides, grid_cell_offset=0.5):
         sx = torch.arange(end=w, device=device, dtype=dtype) + grid_cell_offset  # shift x
         sy = torch.arange(end=h, device=device, dtype=dtype) + grid_cell_offset  # shift y
         sy, sx = torch.meshgrid(sy, sx, indexing='ij')
-        anchor_points.append(torch.stack((sx, sy), -1).view(-1, 2))
+        stacked_points = torch.stack((sx, sy), -1)
+        anchor_points.append(stacked_points.view(-1, 2))
         stride_tensor.append(torch.full((h * w, 1), stride, dtype=dtype, device=device))
     return torch.cat(anchor_points), torch.cat(stride_tensor)
 
@@ -65,10 +110,10 @@ def dist2bbox(distance, anchor_points, xywh=True, dim=-1):
     lt, rb = torch.split(distance, 2, dim)
     x1y1 = anchor_points - lt
     x2y2 = anchor_points + rb
-    if xywh:
-        c_xy = (x1y1 + x2y2) / 2
-        wh = x2y2 - x1y1
-        return torch.cat((c_xy, wh), dim)  # xywh bbox
+    #if xywh:
+    #    c_xy = (x1y1 + x2y2) / 2
+    #    wh = x2y2 - x1y1
+    #    return torch.cat((c_xy, wh), dim)  # xywh bbox
     return torch.cat((x1y1, x2y2), dim)  # xyxy bbox
 
 class Detect(nn.Module):
@@ -80,7 +125,7 @@ class Detect(nn.Module):
     anchors = torch.empty(0)  # init
     strides = torch.empty(0)  # init
     
-    def __init__(self, nc=80, ch=()):  # detection layer
+    def __init__(self, nc=2, ch=()):  # detection layer
         super().__init__()
         self.nc = nc  # number of classes
         self.nl = len(ch)  # number of detection layers
@@ -93,12 +138,12 @@ class Detect(nn.Module):
         self.cv2 = nn.ModuleList(nn.Sequential(Conv(x, c2, 3), Conv(c2, c2, 3), nn.Conv2d(c2, 4 * self.reg_max, 1)) for x in ch)
         self.cv3 = nn.ModuleList(nn.Sequential(Conv(x, c3, 3), Conv(c3, c3, 3), nn.Conv2d(c3, self.nc, 1)) for x in ch)
         self.dfl = DFL(self.reg_max) if self.reg_max > 1 else nn.Identity()
+        
     def forward(self, x):
         shape = x[0].shape  # BCHW
         #print("shape:", shape)
         for i in range(self.nl):
             x[i] = torch.cat((self.cv2[i](x[i]), self.cv3[i](x[i])), 1)
-            print("xi:", x[i].shape)
         if self.training:
             print("test1")
             return x
@@ -108,23 +153,89 @@ class Detect(nn.Module):
             self.shape = shape
             
         box, cls = torch.cat([xi.view(shape[0], self.no, -1) for xi in x], 2).split((self.reg_max * 4, self.nc), 1)
-        #dbox = dist2bbox(self.dfl(box), self.anchors.unsqueeze(0), xywh=True, dim=1) * self.strides
-        return concatenated_tensor
-        #return concatenated_xi
+        
+        dbox = dist2bbox(self.dfl(box), self.anchors.unsqueeze(0), xywh=True, dim=1) * self.strides
+        y = torch.cat((dbox, cls.sigmoid()), 1)
+        return y if self.export else (y, x)
+        
+    def bias_init(self):
+        # Initialize Detect() biases, WARNING: requires stride availability
+        m = self  # self.model[-1]  # Detect() module
+        # cf = torch.bincount(torch.tensor(np.concatenate(dataset.labels, 0)[:, 0]).long(), minlength=nc) + 1
+        # ncf = math.log(0.6 / (m.nc - 0.999999)) if cf is None else torch.log(cf / cf.sum())  # nominal class frequency
+        for a, b, s in zip(m.cv2, m.cv3, m.stride):  # from
+            a[-1].bias.data[:] = 1.0  # box
+            b[-1].bias.data[:m.nc] = math.log(5 / m.nc / (640 / s) ** 2)  # cls (.01 objects, 80 classes, 640 img)
+            
+class Proto(nn.Module):
+    # YOLOv8 mask Proto module for segmentation models
+    def __init__(self, c1, c_=256, c2=32):  # ch_in, number of protos, number of masks
+        super().__init__()
+        self.cv1 = Conv(c1, c_, k=3)
+        self.upsample = nn.ConvTranspose2d(c_, c_, 2, 2, 0, bias=True)  # nn.Upsample(scale_factor=2, mode='nearest')
+        self.cv2 = Conv(c_, c_, k=3)
+        self.cv3 = Conv(c_, c2)
+
+    def forward(self, x):
+        return self.cv3(self.cv2(self.upsample(self.cv1(x))))
+
+class Segment(Detect):
+    # YOLOv8 Segment head for segmentation models
+    def __init__(self, nc=2, nm=32, npr=256, ch=()):
+        super().__init__(nc, ch)
+        self.nm = nm  # number of masks
+        self.npr = npr  # number of protos
+        self.proto = Proto(ch[0], self.npr, self.nm)  # protos
+        self.detect = Detect.forward
+
+        c4 = max(ch[0] // 4, self.nm)
+        self.cv4 = nn.ModuleList(nn.Sequential(Conv(x, c4, 3), Conv(c4, c4, 3), nn.Conv2d(c4, self.nm, 1)) for x in ch)
+
+    def forward(self, x):
+        p = self.proto(x[0])  # mask protos
+        bs = p.shape[0]  # batch size
+
+        #mc_parts = []  # To store intermediate results
+        #for i in range(self.nl):
+             #Apply the convolution and reshape operations
+        #    conv_result = self.cv4[i](x[i])
+        #    reshaped_result = conv_result.view(bs, self.nm, -1)
+        #    # Add the processed result to the list
+        #    mc_parts.append(reshaped_result) 
+        #mc = torch.cat(mc_parts, dim=2)
+
+        mc = torch.cat([self.cv4[i](x[i]).view(bs, self.nm, -1) for i in range(self.nl)], 2)  # mask coefficients
+        print("export:", self.export)
+        print("training:", self.training)
+        print("x01:", x[0].shape)
+        x = self.detect(self, x)
+        print("x02:", x[0].shape)
+        if self.training:
+            return x, mc, p
+        return (torch.cat([x, mc], 1), p) if self.export else (torch.cat([x[0], mc], 1), (x[1], mc, p))
+    
 input_channels = [256, 512, 512]
+
 # Create an instance of the Detect class
 detect_model = Detect(ch=input_channels)
 detect_model.eval()
+segment_model = Segment(ch=input_channels)
+segment_model.eval()
 # Prepare an input tensor
 batch_size = 1 
 
 tensor1 = torch.randn(batch_size, 256, 80, 80)
 tensor2 = torch.randn(batch_size, 512, 40, 40)
-tensor3 = torch.randn(batch_size, 512, 40, 40)
+tensor3 = torch.randn(batch_size, 512, 20, 20)
 # Forward pass through the Detect model
 input_tensors = [tensor1, tensor2, tensor3]
-dbox_output = detect_model(input_tensors)
+#dbox_output = detect_model(input_tensors)
+seg_output = segment_model(input_tensors)
 # Print the shape of the dbox_output tensor
-print("x1:", dbox_output, "shape1",dbox_output.shape)
-#, "x2:", dbox_output[1], "shape2",dbox_output[1].shape, "x3:", dbox_output[2], "shape3",dbox_output[2].shape)
-#print("sy:", dbox_output[1].shape)
+#print("dbox:", dbox_output, "shape1",dbox_output.shape)
+#print("rb:", dbox_output[0], "shapea",dbox_output[0].shape)
+#print("detect1:", seg_output[0], "detectshape1:", seg_output[0].shape,
+#     "detect2:", seg_output[1],)
+print("seg_output:", seg_output[0],"seg_shape:", seg_output[0].shape, "seg_output:", seg_output[1],"seg_shape:")
+
+
